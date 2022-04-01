69025e784098930a29a479f725ab11b9a465c32f
nuri <nurhotun95@mail.ru>
2022-04-01 15:34:57 +0500

    fix config for docker-compose

--- src/task_manager/settings.py
+++ src/task_manager/settings.py
@@ -8,7 +8,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Settings(BaseSettings):
     server_host: str = '127.0.0.1'
     server_port: int = 5000
-    database_url: str = f'sqlite:///{basedir}/sql_app.db'
+    database_url: str = f'sqlite:///{basedir}/../../sql_app.db'
 
     jwt_secret: str
     jwt_algorithm: str = 'HS256'
