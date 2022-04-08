22ab86f6d05563017796cc0e1ced9ac37c8ccaca
nuri <nurhotun95@mail.ru>
2022-04-08 13:30:03 +0500

    add alembic

---
+++ src/task_manager/alembic/versions/2c55cddd7544_create_tables.py
@@ -0,0 +1,24 @@
+"""create tables
+
+Revision ID: 2c55cddd7544
+Revises: 
+Create Date: 2022-04-08 08:35:38.699108
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2c55cddd7544'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
